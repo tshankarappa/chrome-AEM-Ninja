6c3488381fac3b44146bf737518d5349688d82b5
tshankarappa <thilak.shankarappa@gmail.com>
2024-11-01 15:22:59 +0530

    remove GA

--- popup.js
+++ popup.js
@@ -1,19 +1,6 @@
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-122925471-1']);
-_gaq.push(['_trackPageview']);
-
-(function () {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
-function trackButton(e) {
-  _gaq.push(['_trackEvent', e, 'clicked']);
-};
 
 
 
